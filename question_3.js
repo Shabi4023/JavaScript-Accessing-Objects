c79d384cae8596e164da363c4c9cb0d47eeafd34
Shahzaib Meraj <140791911+Shabi4023@users.noreply.github.com>
2024-02-20 12:37:05 +0500

    Add files via upload

---
+++ question_3.js
@@ -0,0 +1,11 @@
+// Create an object named ‘student’ with properties: ‘name’ as ‘Ali’, ‘age’ as 20, and ‘grade’ as ‘A’. Print the name and grade of the student.
+
+
+let student = {
+    name: "Ali",
+    age: 20,
+    grade: "A",
+}
+
+let result = `${student.name} acheived ${student.grade} Grades in the examination.`
+console.log(result);
